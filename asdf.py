c5ebdb86ad67580f4c6f0b5ad618cb5f8d2bb138
kz6fittycent <jimmy.tigert@gmail.com>
2019-01-17 09:12:36 -0600

    Update asdf.py

--- asdf.py
+++ asdf.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
+import time
 
 def main():
     print ("You bored?")
     choice = input ("yes or no?    ").lower()
     if choice.startswith('y'):
        print ("Me, too...")
+       print()
+       time.sleep(1)
+       print("You should watch this video: https://youtu.be/Pr8GD4X35gU")
+       print()
     else:
        print ("Well, leave me alone!")
+       print()
+       time.sleep(1)
+       print()
+       print ("...and go watch this video! https://youtu.be/Pr8GD4X35gU")
+       print()
 
 main()
